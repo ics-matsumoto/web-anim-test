a6d28709a3577f57bca546f76ad2a9701f4986b1
ics-matsumoto <matsumoto@ics-web.jp>
2020-10-22 02:18:10 +0900

    背景・タイトル・ローディングアニメーションを追加

--- src/StartButton.js
+++ src/StartButton.js
@@ -1,7 +1,8 @@
 import { randomWithin } from "./Random.js";
+import { wait } from "./wait.js";
 
 const BTN_WIDTH = 120;
-const BTN_HEIGHT = 30;
+const BTN_HEIGHT = 40;
 
 const createRect = (x = 0, y = 0, w = 0, h = 0) => {
   const elem = document.createElement("div");
@@ -15,63 +16,103 @@ const createRect = (x = 0, y = 0, w = 0, h = 0) => {
 }
 
 export class StartButton {
-  constructor (parent, pos) {
+  constructor (parent, pos, onclick) {
     const root = createRect();
-    const elem = createRect(pos.x, pos.y, BTN_WIDTH, BTN_HEIGHT)
+    const elem = createRect(pos.x, pos.y, BTN_WIDTH, BTN_HEIGHT);
+    const text = createRect(pos.x, pos.y, BTN_WIDTH, BTN_HEIGHT);
     this.root = root;
     this.elem = elem;
+    this.text = text;
     parent.appendChild(root);
     root.appendChild(elem);
-    elem.style.backgroundColor = "gold";
+    root.appendChild(text);
+    elem.style.borderRadius = "2px";
+    elem.style.backgroundColor = "#f7c920";
+    text.style.textAlign = "center";
+    text.style.lineHeight = BTN_HEIGHT + "px";
+    text.innerText = "START";
+    text.style.pointerEvents = "none";
+    text.style.color = "#211136";
 
-    elem.addEventListener("click", () => {
-      this.disappear();
+    elem.addEventListener("click", async () => {
+      await this.disappear();
+      onclick();
+    }, {
+      once: true
     })
   }
 
   async disappear () {
     const elem = this.elem;
+    const text = this.text;
     const screenW = window.innerWidth;
     const screenH = window.innerHeight;
-    const scaleY = screenW / elem.offsetHeight;
-    const scaleX = screenH / elem.offsetWidth;
+    const scaleY = screenW / BTN_HEIGHT * 1.1;
+    const scaleX = screenH / BTN_WIDTH * 1.1;
     await elem.animate([
       {
-        transform: elem.style.transform,
-        backgroundColor: elem.style.backgroundColor
+        transform: elem.style.transform
+      },
+      {
+        transform: elem.style.transform + " scale(0.85)",
+      }] ,{
+        duration: 200,
+        endDelay: 100,
+        fill: "forwards",
+        easing: "cubic-bezier(0,.67,.39,1)",
+      }
+    ).finished;
+    this.text.textContent = "";
+  
+    await elem.animate([
+      {
+        transform: elem.style.transform
       },
       {
         transform: `translate(${screenW / 2}px, ${screenH / 2}px) rotate(90deg) scale(${scaleX}, ${scaleY})`,
-        backgroundColor: "orange"
+      }], {
+        duration: 800,
+        easing: "cubic-bezier(0,.67,.39,1)",
+        fill: "forwards"
       }
-    ], {
-      duration: 800,
-      easing: "ease-out"
-    }).finished;
-    elem.style.visibility = "hidden";
+    ).finished;
 
+    for (let proc = 0; proc <= 100; proc += 2) {
+      this.text.textContent = `loading ... ${proc}%`;
+      await wait(20);
+    }
+    this.text.textContent = "";
+    elem.style.visibility = "hidden";
+  
     const RECT_SIZE = 100;
     const xCount = Math.ceil(screenW / RECT_SIZE);
     const yCount = Math.ceil(screenH / RECT_SIZE);
     const rects = new Array(xCount * yCount).fill(0).map((_, index) => {
       const x = index % xCount;
       const y = (index - x) / xCount;
-      const rect = createRect(x * RECT_SIZE, y * RECT_SIZE, RECT_SIZE, RECT_SIZE);
-      rect.style.backgroundColor = "orange";
+      const rect = createRect(x * RECT_SIZE + RECT_SIZE / 2, y * RECT_SIZE + RECT_SIZE / 2, RECT_SIZE, RECT_SIZE);
+      rect.style.backgroundColor = "#f7c920";
       this.root.appendChild(rect);
       return rect;
     });
 
-    rects.forEach(rect => {
+    rects.forEach((rect, index) => {
+      const x = index % xCount;
+      const y = (index - x) / xCount;
       rect.animate([
         { transform: rect.style.transform },
-        { transform: rect.style.transform + " scale(0)" }
+        { transform: rect.style.transform + " scale(0.01)" }
       ], {
-        duration: 500,
-        delay: randomWithin(500, 1500),
-        easing: "ease-in-out",
+        duration: 800,
+        delay: randomWithin(y * 100, y * 100 + 200),
+        easing: "cubic-bezier(0,.67,.39,1)",
         fill: "forwards"
-      });
-    })
+      })
+        .finished
+        .then(() => {
+          rect.parentNode.removeChild(rect);
+        });
+    });
+
   }
 }
--- src/main.js
+++ src/main.js
@@ -2,14 +2,30 @@
 import { Point } from "./Point.js";
 import { AnimatedStar } from "./AnimatedStar.js";
 import { StartButton } from "./StartButton.js";
+import { createLines } from "./LineBg.js";
 
 const stage = document.getElementById("MainStage");
 
 const showStartScreen = () => {
-  const btn = new StartButton(stage, new Point(300, 500));
+  const pos = new Point(window.innerWidth / 2, 500);
+  const btn = new StartButton(stage, pos, () => {
+    document.getElementById("Top").style.visibility = "hidden";
+    drawBg();
+    initStage();
+  });
 }
 
-const initApp = () => {
+const drawBg = (count = 20) => {
+  createLines(document.getElementById("Background"), [
+    "#131c38",
+    "#201a33",
+    "#23285e",
+    "#1d1c47",
+    "#2e1657"
+  ], count);
+}
+
+const initStage = () => {
   stage.addEventListener("click", async (ev) => {
     const center = new Point(ev.clientX, ev.clientY);
     const star = new AnimatedStar(stage, center);
@@ -17,5 +33,9 @@ const initApp = () => {
   });
 };
 
-showStartScreen();
-// initApp();
+const initApp = () => {
+  drawBg(6);
+  showStartScreen();
+}
+
+initApp();
