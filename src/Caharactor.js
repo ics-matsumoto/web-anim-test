db7911078fc47a8d1f11aaa1022dafb8fc7f38f8
ics-matsumoto <matsumoto@ics-web.jp>
2020-10-23 17:24:51 +0900

    キャラクターアニメーションの直列化

--- src/Caharactor.js
+++ src/Caharactor.js
@@ -12,14 +12,40 @@ export class Charactor {
     const stageW = parent.offsetWidth;
     const stageH = parent.offsetHeight;
     this._x = stageW / 2;
-    this._y = stageH - CHARA_H / 2
+    this._y = stageH - CHARA_H / 2;
+    this._latestAction = null; // 実行中または最後に予約された動作のPromise
     this.charaElem = createRect(this._x, this._y, CHAR_W, CHARA_H);
     this.charaElem.style.backgroundImage = "url(src/assets/tama.svg)";
     this.charaElem.style.transformOrigin = "center bottom";
     parent.appendChild(this.charaElem);
   }
 
+  /**
+   * 複数の動作が重ならないように順番に実行するための処理です。
+   * このメソッド呼び出し時に既にアニメーションが実行中または予約済みの場合、それら全てが完了した後で呼び出し元に制御を戻します。
+   * @returns {Function} ロック解除関数。呼び出し元で動作が完了したら、必ずこの関数をコールします。
+   *  コール時点で後続の動作が実行されます
+   */
+  async _waitAndLock () {
+    const latestAction = this._latestAction; // 一番最後に予約された処理のPromise
+    let unlockFunc;
+    // 新しいPromiseを作成し、「一番最後に予約された処理」にセット
+    this._latestAction = new Promise((resolve, reject) => {
+      unlockFunc = resolve;
+    });
+    await latestAction; // 最初に取得したPromiseのresolveを待つ
+    return unlockFunc; // 新しく作ったPromiseのresolve関数を返す
+  }
+
+  /**
+   * キャラクターを指定の座標までアニメーションを伴って動かします。
+   * アニメーション完了前にこのメソッドを複数回呼び出した場合、呼び出した順番でひとつずつアニメーションを実行します。
+   * @param {Number} x X座標
+   */
   async moveTo (x) {
+    // アニメーションが重ならないように、前の動作が終わるのを待ってロックを取得
+    const unlock = await this._waitAndLock();
+
     const chara = this.charaElem;
     const stageW = chara.parentNode.offsetWidth;
     const dX = x - this._x;
@@ -49,5 +75,7 @@ export class Charactor {
       easing: "ease-out",
     }).finished;
     chara.style.transform = key4.transform;
+
+    unlock(); // ロックを解放
   }
 }
